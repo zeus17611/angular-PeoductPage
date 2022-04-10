e397921e475a8afd5af1dee2f03dba5dd0ddc53f
zeus17611 <parthasarathyrocks10@gmail.com>
2022-04-10 15:39:58 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-g5ju9k
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-g5ju9k)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products = products;
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+  buy(){
+    window.alert('Do ypu wish to purchase');
+  }
+  userRev()
+  {
+    window.alert('4.3 stars');
+  }
+  onNotify()
+  {
+    window.alert('Notified');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
